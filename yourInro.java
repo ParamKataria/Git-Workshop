bd1192578048bc20b93a95ab665c977e18d4bcee
ParamKataria <paramkataria2808@gmail.com>
2025-01-23 19:14:19 -0400

    msg

--- yourInro.java
+++ yourInro.java
@@ -3,8 +3,8 @@ public class yourInro {
     public static void main(String[]args){
 
 
-        System.out.println("Hi I am jai") ;
-        System.out.println("I am 20 yrs old") ;
+        System.out.println("Hi I am Param") ;
+        System.out.println("I am 18 yrs old") ;
     }
 
 
